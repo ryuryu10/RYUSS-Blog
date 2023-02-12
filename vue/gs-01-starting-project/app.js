6ae08eba07668aca9fc1e74fdd20cb1f4b7359ae
RyuSeoungWoo <ryuryu10@naver.com>
2023-02-13 07:43:41 +0900

    vue 1-7

--- vue/gs-01-starting-project/app.js
+++ vue/gs-01-starting-project/app.js
@@ -1,1 +1,13 @@
-Vue.createApp({});
+Vue.createApp({
+    data () {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal () {
+            this.goals.push(this.enteredValue); 
+        }
+    }
+}).mount('#app'); 
