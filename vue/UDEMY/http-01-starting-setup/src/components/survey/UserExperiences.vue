0092edf2e39dc5b15fb02a11f201c031ccc22c61
RyuSeoungWoo <ryuryu10@naver.com>
2023-02-19 15:37:05 +0900

    vue - http

---
+++ vue/UDEMY/http-01-starting-setup/src/components/survey/UserExperiences.vue
@@ -0,0 +1,70 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences">Load Submitted Experiences</base-button>
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <ul v-if="!isLoading">
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false
+    }
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      fetch('https://vue-http-41b1d-default-rtdb.firebaseio.com/surveys.json').then(
+        (response) => {
+          if (response.ok) {
+            return response.json()
+          }
+        }
+      ).then((data) => {
+        this.isLoading = false;
+        const result = [];
+        for (const id in data) {
+          result.push({ 
+            id: id ,
+            name: data[id].name,
+            rating: data[id].rating,
+          });
+        }
+        this.results = result;
+      })
+      .catch(() => {});
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
